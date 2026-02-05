8c9a0fa8b2ab2812390617cdc3f9a38eb5ff541a
Namrata Dhakal <namru009@gmail.com>
2026-02-05 19:35:04 +0545

    adding task1_variable file

---
+++ task1_variables.py
@@ -0,0 +1,27 @@
+Full_name = "Krish Khanal"
+Age = 24
+CGPA = 3.56
+Current_semester = "5th sem"
+Enrollment_status = True
+
+
+print(type(Full_name))
+print(type(Age))
+print(type(CGPA))
+print(type(Current_semester))
+print(type(Enrollment_status))
+
+## reassignment variable Age :
+Age = "twenty four"
+print(type(Age))
+
+## 1. How does Python’s dynamic typing influence memory allocation and runtime behavior?
+
+#  Ans:  Dynamic typing means the type of variable is associated with runtime values and not with variables itself. Memory is allocated for objects stored on the heap with extra data and  uses more memory. Also Type checking happens during execution which makes operation slower than in statically typed languages.
+
+
+# 2. Why is strict type enforcement preferred in system-level software but not in scripting languages? 
+
+# Ans: As system-level software focuses on improving performance, safety and reliability strict type enforcement is preferred. But scripting languages avoid it to stay flexible and easy to write making development faster and simple.
+
+
